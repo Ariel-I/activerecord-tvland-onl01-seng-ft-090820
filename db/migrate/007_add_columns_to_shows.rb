c3cac89030d34d88fc2d3c60434abd28832f33b2
Ariel I <arielibarra39@gmail.com>
2020-10-14 22:03:24 +0000

    Automatically backed up by Learn

--- db/migrate/007_add_columns_to_shows.rb
+++ db/migrate/007_add_columns_to_shows.rb
@@ -2,7 +2,7 @@ class AddColumnsToShows <  ActiveRecord::Migration[5.1]
   
   def change
     add_column :shows, :genre, :string 
-    add_column :shows, :day, :integer 
+    add_column :shows, :day, :string 
     add_column :shows, :season, :string 
   end 
   
